207ac50c0dcb2a429163a7810c95712fb0f9c987
amiedawn <amie.d.carroll@gmail.com>
2020-12-14 18:14:56 -0700

    connect to the database

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // import express
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 
 // add PORT designation and app expression
 const PORT = process.env.PORT || 3001;
@@ -9,12 +10,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+// Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+  if (err) {
+    return console.error(err.message);
+  }
+
+  console.log('Connected to the election database.');
+});
+
 // default response for any other request(not found) catch all
+// has to go after all GET statements, after middleware, before listener
 app.use((req, res) => {
   res.status(404).end();
 });
 
-// start the express.js server on port 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Start server after DB connection
+db.on('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
